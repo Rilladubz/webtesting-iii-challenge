f7c927596839d14b165af60205717db3203cebaa
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2019-11-11 19:01:15 -0500

    final commit

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Display from './Display';
+
+
+
+test('Display renders correctly', () => {
+    expect(render(<Display />)).toMatchSnapshot();
+  });
+
+
+
